9ae09e0438a77c88a5cb0d7d6c04a84f784666ad
zsofia <50777550+zsofia85@users.noreply.github.com>
2020-04-21 15:43:54 +0200

    JS added for popup

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+/* Portfolio_V1
+mmdi - 0919 
+Zsofia Csagoly */
+
+/* REF - Build a Popup With JavaScript
+https://www.youtube.com/watch?v=MBaw_6cPmAw */
+
+const openPopupButtons = document.querySelectorAll ('[data-popup-target]')
+const closePopupButtons = document.querySelectorAll ('[data-close-button')
+const overlay = document.getElementById('overlay')
+
+
+openPopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup = document.querySelector(button.dataset.popupTarget)
+        openPopup(popup)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const popup = document.querySelectorAll('.popup.active')
+    popup.forEach(popup => {
+        closePopup(popup)
+    })
+})
+
+closePopupButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const popup = button.closest ('.popup')
+        closePopup(popup)
+    })
+})
+
+function openPopup(popup) {
+    if (popup == null) return
+    popup.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closePopup(popup) {
+    if (popup == null) return
+    popup.classList.remove('active')
+    overlay.classList.remove('active')
+}
